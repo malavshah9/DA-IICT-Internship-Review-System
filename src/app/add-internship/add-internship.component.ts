ddd2bc3757fcdc5cbfaa520fc8996037720049db
jaishilbhavsar <jaishilbhavsar@yahoo.in>
2019-11-20 12:18:15 +0530

    select Faculty Step added in add_internship

--- src/app/add-internship/add-internship.component.ts
+++ src/app/add-internship/add-internship.component.ts
@@ -16,6 +16,8 @@ export class AddInternshipComponent implements OnInit {
   selectRecruiterFormGroup:FormGroup;
   selectCompanyFormGroup:FormGroup;
   selectJobLocationFormGroup:FormGroup;
+  selectFacultyFormGroup:FormGroup;
+  isCompany:boolean=false;
   ngOnInit() {
     this.selectRecruiterFormGroup = this._formBuilder.group({
       recruiters: ['', Validators.required]
@@ -23,11 +25,28 @@ export class AddInternshipComponent implements OnInit {
     this.selectCompanyFormGroup = this._formBuilder.group({
       companyName: ['', Validators.required]
     });
+    this.selectFacultyFormGroup = this._formBuilder.group({
+      facultyName: ['', Validators.required]
+    });
     this.selectJobLocationFormGroup = this._formBuilder.group({
       selectedLocation: ['', Validators.required],
       selectedProfile:['',Validators.required],
       selectedInternshipType:['',Validators.required]
     });
+
+
+  }
+  recruiterSubmit()
+  {
+    let val=this.selectRecruiterFormGroup.value.recruiters;
+    if(val=="company")
+    {
+      this.isCompany=true;
+    }
+    else
+    {
+      this.isCompany=false;
+    }
   }
 
 }
